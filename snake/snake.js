d20bdaf49c428fae5a36242bc48eeb70102f43dd
siigdev <sebbahlol@gmail.com>
2018-12-30 23:56:46 +0100

    Tail working

--- snake/snake.js
+++ snake/snake.js
@@ -2,7 +2,7 @@ window.onload = function setup() {
   c = document.getElementById("snakeCanvas");
   height = c.height;
   width = c.width;
-  scale = 10;
+  scale = 20;
   ctx = c.getContext("2d");
 
   snake = new Snake();
@@ -23,6 +23,7 @@ function draw() {
   }
   fruit.draw();
   snake.update();
+  snake.collision();
   snake.draw();
 }
 function controls(key) {
@@ -48,7 +49,7 @@ class Fruit {
   }
   draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(this.x, this.y, 10, 10);
+    ctx.fillRect(this.x, this.y, scale, scale);
   }
   spawn() {
     this.x = Math.floor((Math.random() * width) / scale + 0) * scale;
@@ -58,23 +59,27 @@ class Fruit {
 
 class Snake {
   constructor() {
-    this.x = 1 * scale;
-    this.y = 1 * scale;
+    this.point = 0;
+    this.x = width / 2;
+    this.y = height / 2;
     this.xspeed = 0;
     this.yspeed = 0;
     this.tail = [];
-    this.point = 0;
   }
   update() {
-    if (this.x > c.width || this.y > c.height || this.y < 0 || this.x < 0) {
-        this.tail = [];
-        this.point = 0;
-      }
+    this.collision();
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
+    }
+    this.tail[this.point - 1] = { x: this.x, y: this.y };
     this.x += this.xspeed;
     this.y += this.yspeed;
   }
   draw() {
     ctx.fillStyle = "lime";
+    for (var i = 0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+    }
     ctx.fillRect(this.x, this.y, scale, scale);
   }
   eat() {
@@ -84,8 +89,23 @@ class Snake {
     }
     return false;
   }
-  dead() {
-    
+  collision() {
+    if (this.x > c.width || this.y > c.height || this.y < 0 || this.x < 0) {
+      this.reset();
+    }
+    for (let i = 0; i < this.tail.length; i++) {
+      if (this.tail[i].x == this.x && this.tail[i].y == this.y) {
+        this.reset();
+      }
+    }
+  }
+  reset() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    this.point = 0;
+    this.tail = [];
   }
   setspeed(x, y) {
     this.xspeed = x * scale;
