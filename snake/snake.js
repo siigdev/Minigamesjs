dd4c08df12ca5861dd449ebb6e06224e2b8985e9
siigdev <sebbahlol@gmail.com>
2018-12-30 22:33:56 +0100

    Reconstructured snake

--- snake/snake.js
+++ snake/snake.js
@@ -1,67 +1,92 @@
-window.onload=function(){
-    c = document.getElementById("snakeCanvas");
-    ctx = c.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,c.width,c.height);
+window.onload = function setup() {
+  c = document.getElementById("snakeCanvas");
+  height = c.height;
+  width = c.width;
+  scale = 10;
+  ctx = c.getContext("2d");
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, width, height);
+  snake = new Snake();
+  fruit = new Fruit();
+  fruit.spawn();
+  setInterval(draw, 1000/15);
+  document.addEventListener("keydown", controls);
+};
 
-    scale = 20;
-    rows = c.height/scale;
-    cols = c.width/scale;
-
-    snake = new Snake();
-    //food = new Food();
-    window.addEventListener("keydown", snake.controls);
-
-    setInterval(snakeGame,1000/15);
+function draw() {
+  if (snake.dead()){
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, width, height);
+  }
+  if (snake.eat(fruit)){
+      fruit.spawn();
+  }
+  fruit.draw();
+  snake.update();
+  snake.draw();
 }
-
-function snakeGame(){
-    snake.update();
-    snake.draw();
-    //food.draw();
-}
-
-class Snake{
-    constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.newx = scale * 1;
-        this.newy = 0;
-        this.tail = [];
-        this.points = 0;
+function controls(key){
+    switch (key.key){
+        case "ArrowUp":
+            snake.setspeed(0,-1);
+            break;
+        case "ArrowDown":
+            snake.setspeed(0,1);
+            break;
+        case "ArrowLeft":
+            snake.setspeed(-1,0);
+            break;
+        case "ArrowRight":
+            snake.setspeed(1,0);
+            break;
     }
-
+}
+class Fruit {
+    constructor() {
+        this.fruitx;
+        this.fruity;
+    }
     draw(){
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.x, this.y, scale, scale);
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y, 10, 10);
     }
-    update(){
-        this.x += this.newx;
-        this.y += this.newy;
-        console.log(this.newy);
-        
+    spawn(){
+        this.x = Math.floor((Math.random()*width/scale)+0)*scale;
+        this.y = Math.floor((Math.random()*height/scale)+0)*scale;
     }
-    controls(key){
-        switch (key.key){
-            case "ArrowUp":
-                this.newx = 0;
-                this.newy = -scale;
-                console.log(this.newy);
-                break;
-            case "ArrowDown":
-                this.newx = 0;
-                this.newy = +scale;
-                break;
-            case "ArrowLeft":
-                this.newx = -scale;
-                this.newy = 0;
-                break;
-            case "ArrowRight":
-                this.newx = +scale;
-                this.newy = 0;
-                break;
-        }
-    }
+}
 
+class Snake {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    this.tail = [];
+  }
+  update() {
+    this.x += this.xspeed;
+    this.y += this.yspeed;
     
+  }
+  draw() {
+    ctx.fillStyle = "lime";
+    ctx.fillRect(this.x, this.y, 10, 10);
+  }
+  eat(){
+      if(this.x == fruit.x && this.y == fruit.y) {
+          this.point++;
+          return true;
+      }
+      return false;
+  }
+  dead(){
+    if (this.x > c.width || this.y > c.height || this.y < 0 || this.x < 0){
+        return true;
+    }
+  }
+  setspeed(x, y){
+      this.xspeed = x*scale;
+      this.yspeed = y*scale;
+  }
 }
