f0878cc9922e5c6e7368aa7993f6b131a4844453
siigdev <sebbahlol@gmail.com>
2019-03-18 00:01:55 +0100

    draw and move spaceship

--- Asteroids/asteroids.js
+++ Asteroids/asteroids.js
@@ -4,22 +4,42 @@ window.onload = function setup() {
     width = c.width;
     ctx = c.getContext("2d");
   
-    Ship = new Ship();
+    ship = new Ship();
     setInterval(draw, 1000 / 15);
 }
 function draw() {
     ctx.fillStyle = "#222";
     ctx.fillRect(0, 0, width, height);
+    ship.update();
+    ship.draw();
 }
+function controls(key) {
+    switch (key.key) {
+      case "ArrowUp":
+        ship.accelerate();
+        break;
+      case "ArrowDown":
+        snake.decelerate();
+        break;
+      case "ArrowLeft":
+        snake.turn(left);
+        break;
+      case "ArrowRight":
+        snake.turn(right);
+        break;
+    }
+  }
 
 class Asteroid {
     constructor() {
-        this.x;
-        this.y;
-        this.maxspeed;
-        this.acceleration;
+        this.x = 10;
+        this.y = 10;
+
     }
     draw() {
+
+    }
+    update() {
 
     }
 
@@ -27,12 +47,29 @@ class Asteroid {
 
 class Ship {
     constructor() {
-        this.x;
-        this.y;
+        this.x = width/2;
+        this.y = height/2;
+        this.maxspeed;
+        this.acceleration;
+        this.heading = 30;
     }
     draw() {
-
+        ctx.save();
+        ctx.rotate(this.heading*Math.PI/180);
+        ctx.strokeStyle = "#fff";
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x+10, this.y+20);
+        ctx.lineTo(this.x, this.y+15);
+        ctx.lineTo(this.x, this.y+15);
+        ctx.lineTo(this.x-10, this.y+20);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.restore();
     }
-
+    update() {
+        this.x ++;
+        this.y ++;
+    }
 }
 
