91ab4ae87816638ab619e9a1cfb1f70eb2bd43bc
siigdev <sebbahlol@gmail.com>
2019-03-18 22:54:11 +0100

    ship moving direction

--- Asteroids/asteroids.js
+++ Asteroids/asteroids.js
@@ -5,30 +5,30 @@ window.onload = function setup() {
     ctx = c.getContext("2d");
   
     ship = new Ship();
-    setInterval(draw, 1000 / 15);
+    document.addEventListener("keydown", controls);
+    setInterval(draw, 1000 / 50);
 }
 function draw() {
+    ctx.save();
     ctx.fillStyle = "#222";
     ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+
+    ctx.save();
     ship.update();
     ship.draw();
+    ctx.restore();
 }
 function controls(key) {
-    switch (key.key) {
-      case "ArrowUp":
+    if (key.key == "ArrowUp")
         ship.accelerate();
-        break;
-      case "ArrowDown":
-        snake.decelerate();
-        break;
-      case "ArrowLeft":
-        snake.turn(left);
-        break;
-      case "ArrowRight":
-        snake.turn(right);
-        break;
+    if (key.key == "ArrowDown")
+        ship.decelerate();
+    if (key.key == "ArrowLeft")
+        ship.turn(1);
+    if (key.key == "ArrowRight")
+        ship.turn(0);
     }
-  }
 
 class Asteroid {
     constructor() {
@@ -51,25 +51,35 @@ class Ship {
         this.y = height/2;
         this.maxspeed;
         this.acceleration;
-        this.heading = 30;
+        this.angle = 0;
+        this.speed = 5;
     }
     draw() {
-        ctx.save();
-        ctx.rotate(this.heading*Math.PI/180);
+        ctx.translate(this.x,this.y);
+        ctx.rotate((this.angle+90) * Math.PI/180);
         ctx.strokeStyle = "#fff";
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x+10, this.y+20);
-        ctx.lineTo(this.x, this.y+15);
-        ctx.lineTo(this.x, this.y+15);
-        ctx.lineTo(this.x-10, this.y+20);
+        ctx.moveTo(0, 0);
+        ctx.lineTo(10, 20);
+        ctx.lineTo(0, 15);
+        ctx.lineTo(0, 15);
+        ctx.lineTo(-10, 20);
         ctx.closePath();
         ctx.stroke();
-        ctx.restore();
     }
     update() {
-        this.x ++;
-        this.y ++;
+    }
+    accelerate() {
+      this.x += this.speed * Math.cos(this.angle * Math.PI / 180);
+      this.y += this.speed * Math.sin(this.angle * Math.PI / 180);
+    }
+    turn(heading) {
+      if(heading == 1) {
+        this.angle = this.angle+10;
+      }
+      else if (heading == 0){
+        this.angle = this.angle-10;
+      }
     }
 }
 
